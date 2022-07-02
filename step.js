7ece3d4fd13bb726776806ed5a47cad4524fc878
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-07-02 17:03:10 +0700

    Update step.js

--- step.js
+++ step.js
@@ -1,41 +1,73 @@
-// ------------register-steps--------------
-$(document).ready(function () {
-    $('.nav-tabs > li a[title]').tooltip();
-    //Wizard
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        var $target = $(e.target);
-        if ($target.hasClass('disabled')) {
-            return false;
-        }
-    });
+<!-- Created By CodingNepal -->
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
 
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-        var $target = $(e.target);
-        $target.parent().addClass('active');
-    });
-
-    $('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
-        var $target = $(e.target);
-        $target.parent().removeClass('active');
-    });
-
-
-    $(".next-step").click(function (e) {
-        var $active = $('.wizard .nav-tabs li a.active');
-        $active.parent().next().children().removeClass('disabled');
-        $active.parent().addClass('done');
-        nextTab($active);
-    });
-
-    $(".prev-step").click(function (e) {
-        var $active = $('.wizard .nav-tabs li a.active');
-        prevTab($active);
-    });
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
 });
 
-function nextTab(elem) {
-    $(elem).parent().next().find('a[data-toggle="tab"]').click();
-}
-function prevTab(elem) {
-    $(elem).parent().prev().find('a[data-toggle="tab"]').click();
-}
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
